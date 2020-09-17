ce7bba7c5a21de30ddc16f1f0b73429215ab885e
jpm <jpmckenna90@gmail.com>
2020-09-17 17:35:20 -0400

    Installed date-fns to find current week, using this to populate a current adjective for bio.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,70 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+// import getISOWeek from "date-fns";
+import getISOWeek from "date-fns/getISOWeek";
+import getWeek from "date-fns/getWeek";
 import "./About.css";
 
 function About() {
   const weeklyAdjective = () => {
-    const adjectives = ["fat", "perfect", "real", "actual", "biological"];
-    // console.log(Math.floor(Math.random(adjectives.length)));
-    // console.log(Math.random(adjectives.length));
-    console.log(Math.floor(Math.random() * adjectives.length - 1));
-    return adjectives[Math.floor(Math.random() * adjectives.length - 1)];
+    const adjectives = [
+      "fat",
+      "perfect",
+      "real",
+      "actual",
+      "biological",
+      "attractive",
+      "bald",
+      "beautiful",
+      "chubby",
+      "clean",
+      "dazzling",
+      "drab",
+      "elegant",
+      "fancy",
+      "fit",
+      "flabby",
+      "glamorous",
+      "gorgeous",
+      "handsome",
+      "long",
+      "magnificent",
+      "muscular",
+      "plain",
+      "plump",
+      "quaint",
+      "scruffy",
+      "shapely",
+      "short",
+      "skinny",
+      "stocky",
+      "ugly",
+      "unkempt",
+      "unsightly",
+      "alive",
+      "careful",
+      "clever",
+      "dead",
+      "easy",
+      "famous",
+      "gifted",
+      "hallowed",
+      "helpful",
+      "important",
+      "inexpensive",
+      "mealy",
+      "mushy",
+      "odd",
+      "powerful",
+      "rich",
+      "shy",
+      "jolly",
+      "puny",
+      "chonky",
+    ];
+
+    let result = getWeek(new Date(2020, 1, 1));
+    return adjectives[result];
   };
 
   return (
